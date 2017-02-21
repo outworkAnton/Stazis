2a633260008dcc4fa839fed9adc2a22a1262340d
outworkAnton <outwork.anton@gmail.com>
2017-02-21 15:46:02 +0300

    add record form

---
+++ AddRecord.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Stazis
+{
+	/// <summary>
+	/// Description of AddRecord.
+	/// </summary>
+	public partial class AddRecord : Form
+	{
+		public AddRecord()
+		{
+			InitializeComponent();
+		}
+	}
+}
